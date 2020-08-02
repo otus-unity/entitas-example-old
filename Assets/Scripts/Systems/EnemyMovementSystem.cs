0f54941c92f4c9ffe1fcc7f41b3591a2000f2af5
Nikolay Zapolnov <zapolnov@gmail.com>
2020-08-02 09:00:04 +0300

    Jobs demo.

--- Assets/Scripts/Systems/EnemyMovementSystem.cs
+++ Assets/Scripts/Systems/EnemyMovementSystem.cs
@@ -2,27 +2,54 @@
 using System.Collections.Generic;
 using Entitas;
 using UnityEngine;
+using Unity.Collections;
+using Unity.Jobs;
 
 public class EnemyMovementSystem : IExecuteSystem, ICleanupSystem
 {
+    struct EnemyData
+    {
+        public Vector2 position;
+        public float angle;
+    }
+
+    struct UpdateJob : IJobParallelFor
+    {
+        public float deltaTime;
+        public NativeArray<EnemyData> enemyData;
+
+        public void Execute(int index)
+        {
+            var data = enemyData[index];
+
+            var angle = data.angle * Mathf.Deg2Rad;
+            var dir = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+            data.position += dir * 5.0f * deltaTime;
+
+            enemyData[index] = data;
+        }
+    }
+
     Contexts contexts;
     IGroup<GameEntity> entities;
     List<(Vector2 position, float health)> clones = new List<(Vector2, float)>();
+    UpdateJob updateJob;
 
     public EnemyMovementSystem(Contexts contexts)
     {
         this.contexts = contexts;
         entities = contexts.game.GetGroup(GameMatcher.Enemy);
+        updateJob = new UpdateJob();
     }
 
     public void Execute()
     {
-        int enemyCount = 0;
+        int enemyCount = entities.count;
+        var enemyArray = new NativeArray<EnemyData>(enemyCount, Allocator.TempJob, NativeArrayOptions.UninitializedMemory);
 
         clones.Clear();
+        int i = 0;
         foreach (var e in entities) {
-            ++enemyCount;
-
             e.enemy.timeUntilNextTurn -= Time.deltaTime;
             if (e.enemy.timeUntilNextTurn <= 0.0f) {
                 e.ReplaceRotation(Random.Range(0.0f, 360.0f));
@@ -32,10 +59,20 @@ public class EnemyMovementSystem : IExecuteSystem, ICleanupSystem
                     clones.Add((e.position.value, e.health.value));
             }
 
-            var angle = e.rotation.angle * Mathf.Deg2Rad;
-            var dir = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
-            e.ReplacePosition(e.position.value + dir * 5.0f * Time.deltaTime);
+            enemyArray[i++] = new EnemyData{ position = e.position.value, angle = e.rotation.angle };
         }
+
+        updateJob.deltaTime = Time.deltaTime;
+        updateJob.enemyData = enemyArray;
+
+        JobHandle jobHandle = updateJob.Schedule(enemyCount, 1);
+        jobHandle.Complete();
+
+        i = 0;
+        foreach (var e in entities)
+            e.ReplacePosition(enemyArray[i++].position);
+
+        enemyArray.Dispose();
 
         foreach (var clone in clones) {
             var cloneEntity = contexts.game.CreateEntity();
